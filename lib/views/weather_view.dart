9919133563fc20957d2985183ecc0ac7c2225679
Rajab-Alkahef <rajabalkahef3@gmail.com>
2023-10-12 10:31:36 +0300

    create weather card in weather page

--- lib/views/weather_view.dart
+++ lib/views/weather_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:gradient_borders/box_borders/gradient_box_border.dart';
+import 'package:gradient_borders/input_borders/gradient_outline_input_border.dart';
 
 class WeatherView extends StatelessWidget {
   const WeatherView({super.key});
@@ -11,17 +13,102 @@ class WeatherView extends StatelessWidget {
               begin: Alignment.bottomLeft,
               end: Alignment.topRight,
               colors: [Color(0xff0d2843), Color(0xff144875)])),
-      child: const Scaffold(
+      child: Scaffold(
         resizeToAvoidBottomInset: false,
-        /* appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-          title: const Text('weather'),
-        ),
-     */
         backgroundColor: Colors.transparent,
-        // bottomNavigationBar: CustomButtomNavigationBar(),
+        body: Stack(
+          children: [
+            Align(
+              alignment: const Alignment(0, -0.5),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16),
+                  color: const Color(0xff194f7f),
+                  border: gradiantBorder(),
+                ),
+                height: 250,
+                width: 300,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    const Text(
+                      "Alexandria",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 36,
+                          fontFamily: 'Montserrat'),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          '18°',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 60,
+                              fontFamily: 'Montserrat'),
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        Container(height: 55, width: 2, color: Colors.white),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                              color: const Color(0xff35648f),
+                              borderRadius: BorderRadius.circular(16)),
+                          width: 100,
+                          height: 50,
+                          child: const Center(
+                            child: Text(
+                              'Cloudy',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontFamily: 'Montserrat',
+                                  fontSize: 24),
+                            ),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+            const Positioned(
+              top: 300,
+              left: 125,
+              child: Icon(
+                Icons.cloud,
+                color: Colors.white,
+                size: 150,
+              ),
+            ),
+          ],
+        ),
       ),
+    );
+  }
+
+  GradientBoxBorder gradiantBorder() {
+    return const GradientBoxBorder(
+      gradient: LinearGradient(
+        begin: Alignment.centerLeft,
+        end: Alignment.topRight,
+        colors: [
+          Color.fromARGB(255, 38, 114, 190),
+          Color.fromARGB(255, 45, 148, 239),
+        ],
+      ),
+      width: 1.5,
     );
   }
 }
